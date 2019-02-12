8f8949ad34dc5de4d86e1f82ff14ee75091a32fd
Sasha Alex Romanenko <alex@alex-tech-adventures.com>
2019-02-12 00:14:06 -0500

    3. Convert to template. Add style.

---
+++ src/ui/panel.css.js
@@ -0,0 +1,17 @@
+export const style = `
+/*! CSS Used from: http://localhost:63342/web-components-tutorial/src/css/adminlte.css */
+*,*::before,*::after{box-sizing:border-box;}
+h5{margin-top:0;margin-bottom:0.5rem;}
+h5{margin-bottom:0.5rem;font-family:inherit;font-weight:500;line-height:1.2;color:inherit;}
+h5{font-size:1.25rem;}
+.card{position:relative;display:flex;flex-direction:column;min-width:0;word-wrap:break-word;background-color:#ffffff;background-clip:border-box;border:0 solid rgba(0, 0, 0, 0.125);border-radius:0.25rem;}
+.card-body{flex:1 1 auto;padding:1.25rem;}
+.card-header{position:relative;padding:0.75rem 1.25rem;margin-bottom:0;background-color:#007bff;color:#ffffff;border-bottom:1px solid rgba(0, 0, 0, 0.125);border-top-left-radius:0.25rem;border-top-right-radius:0.25rem;}
+.card-header:first-child{border-radius:calc(0.25rem - 0) calc(0.25rem - 0) 0 0;}
+.m-0{margin:0!important;}
+.card{margin-bottom:1rem!important;}
+@media print{
+*,*::before,*::after{text-shadow:none!important;box-shadow:none!important;}
+}
+.card{box-shadow:0 0 1px rgba(0, 0, 0, 0.125), 0 1px 3px rgba(0, 0, 0, 0.2);}
+.card-header::after,.card-body::after{display:block;clear:both;content:"";}`;
--- src/ui/panel.js
+++ src/ui/panel.js
@@ -1,25 +1,30 @@
-class Panel extends HTMLElement
-{
-    constructor() {
-        super();
+import {style} from "./panel.css.js";
 
-        this.attachShadow({mode: "open"});
-        this.shadowRoot.innerHTML =
-            `
-<div class="card card-primary card-outline"
+const panelTpl = document.createElement('template');
+panelTpl.innerHTML = `
+<style>
+${style}
+</style>
+<div class="card card-primary card-outline">
         <div class="card-header">
           <h5 class="m-0">Featured</h5>
         </div>
         <div class="card-body">
-          <h6 class="card-title">Special title treatment</h6>
-
           
         </div>
         <div class="card-footer">
           
         </div>
 </div>
-            `
+`;
+
+class Panel extends HTMLElement
+{
+    constructor() {
+        super();
+
+        this.attachShadow({mode: "open"});
+        this.shadowRoot.appendChild(panelTpl.content.cloneNode(true));
     }
 }
 
