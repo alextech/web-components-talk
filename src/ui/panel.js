d6be6ecf3510b55662cdd8f701016cdc0b0df609
Sasha Alex Romanenko <alex@alex-tech-adventures.com>
2019-02-11 23:57:40 -0500

    2. Attach shadow root

--- src/ui/panel.js
+++ src/ui/panel.js
@@ -3,6 +3,23 @@ class Panel extends HTMLElement
     constructor() {
         super();
 
+        this.attachShadow({mode: "open"});
+        this.shadowRoot.innerHTML =
+            `
+<div class="card card-primary card-outline"
+        <div class="card-header">
+          <h5 class="m-0">Featured</h5>
+        </div>
+        <div class="card-body">
+          <h6 class="card-title">Special title treatment</h6>
+
+          
+        </div>
+        <div class="card-footer">
+          
+        </div>
+</div>
+            `
     }
 }
 
