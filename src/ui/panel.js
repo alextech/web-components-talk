329d96dab8449cf6b522caa513d2171d6d4a24ba
Sasha Alex Romanenko <alex@alex-tech-adventures.com>
2019-02-12 00:37:34 -0500

    4. Header attribute

--- src/ui/panel.js
+++ src/ui/panel.js
@@ -7,7 +7,7 @@ ${style}
 </style>
 <div class="card card-primary card-outline">
         <div class="card-header">
-          <h5 class="m-0">Featured</h5>
+          <h5 class="m-0"></h5>
         </div>
         <div class="card-body">
           
@@ -20,11 +20,30 @@ ${style}
 
 class Panel extends HTMLElement
 {
+    static get observedAttributes() {
+        return ['header'];
+    }
+
     constructor() {
         super();
 
         this.attachShadow({mode: "open"});
         this.shadowRoot.appendChild(panelTpl.content.cloneNode(true));
+
+        this.headerNode = this.shadowRoot.querySelector(".card .card-header h5");
+    }
+
+    attributeChangedCallback(attrName, oldVal, newVal) {
+        if (oldVal === newVal) {
+            return;
+        }
+
+        switch (attrName) {
+            case 'header':
+                this.headerNode.innerText = newVal;
+
+                break;
+        }
     }
 }
 
